8a124ad371241657be2d0ce4a22b34516f9a1375
uhei <50701988+kyohayata@users.noreply.github.com>
2020-05-27 10:33:56 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,59 @@
+//ここからパララックス
+var window_h = $(window).height();
+$("#wh span").text(window_h);
+//スクロールイベント
+$(window).on("scroll", function() {
+  var scroll_top = $(window).scrollTop();
+  $("#scroll span").text(scroll_top);
+
+  //見出しが左から現れる
+  $(".left").each(function() {
+    var elem_pos = $(this).offset().top;
+    $(this).find(".box_pos span").text(Math.floor(elem_pos));
+  
+    //どのタイミングでフェードインさせるか
+    if (scroll_top >= elem_pos - window_h+100) {
+      $(this).addClass("left-fadein");
+    } else {
+      $(this).removeClass("left-fadein");
+    }
+  });
+
+  //説明文と画像が右から現れる
+  $(".right").each(function() {
+    var elem_pos1 = $(this).offset().top;
+    $(this).find(".box_pos span").text(Math.floor(elem_pos1));
+  
+    //どのタイミングでフェードインさせるか
+    if (scroll_top >= elem_pos1 - window_h+150) {
+      $(this).addClass("right-fadein");
+    } else {
+      $(this).removeClass("right-fadein");
+    }
+  });
+});
+//ここまでパララックス
+
+
+//ハンバーガーメニュー
+$(function() {
+  $('.navToggle').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+          $('.logo').css('display','none');
+          $('header').css('padding','5px 0');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+          $('.logo').css('display','block');
+          $('header').css('padding','30px 0');
+      }
+  });
+});
+
+
+
+
+
+
